2c95a7f6642b2aa28e2808067a7cb08b138ec11b
erico1 <oko15001@byui.edu>
2019-02-25 03:16:56 -0700

    correct

--- assignment/lesson-7/script/windchill.js
+++ assignment/lesson-7/script/windchill.js
@@ -10,7 +10,7 @@
 let x = parseInt(document.getElementById('speed').innerHTML);
 let y = parseInt(document.getElementById('temp').innerHTML);
 
-let windChil_factor = (35.74 + (0.6125 * y) - (35.75 *(Math.pow( x, 0.16))) + 0.4275 *y*(Math.pow(y,0.16)))
+let windChil_factor = (35.74 + (0.6125 * y)) - (35.75 *(Math.pow( x, 0.16))) + 0.4275 *y*(Math.pow(x,0.16))
 
 // Output - round to one decimal and output string with HTML to innerHTML of a div
 document.getElementById('output').innerHTML = "The result is <strong>" + windChil_factor.toFixed(1) + '78&#8457;' +"</strong>";
