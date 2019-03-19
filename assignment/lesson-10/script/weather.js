0d7047aac952988356ac876a2249048178440880
erico1 <oko15001@byui.edu>
2019-03-19 11:47:24 -0600

    bhjj

--- assignment/lesson-10/script/weather.js
+++ assignment/lesson-10/script/weather.js
@@ -8,8 +8,11 @@ weatherRequest.onload = function ()
 
     let weatherData = JSON.parse(weatherRequest.responseText)
     console.log(weatherData);
-    document.getElementById('currentTemp').innerHTML = weatherData.main.temp;
-    document.getElementsByClassName('cT').innerHTML = weatherData.weather.description
+    document.getElementById('temMax').innerHTML = weatherData.main.temp_max;
+    document.getElementById('des').innerHTML = weatherData.weather[0].description;
+    document.getElementById('humidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('speed').innerHTML = weatherData.wind.speed;
+    
 
     let icon = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png' ;
     let desc = weatherData.weather[0].description;
