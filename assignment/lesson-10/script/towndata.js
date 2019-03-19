9bcfe5e8bf45b5af3d3713d2f696334239e788cb
erico1 <oko15001@byui.edu>
2019-03-19 04:12:19 -0600

    apiTest

---
+++ assignment/lesson-10/script/towndata.js
@@ -0,0 +1,60 @@
+var section = document.querySelector('section');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var towns = request.response;
+    showtownsInfo(towns);
+}
+
+function showtownsInfo(jsonObj) {
+    var towns = jsonObj['towns'];
+    for (var i = 0; i < towns.length; i++) {
+        if (towns[i].name == "Franklin" || towns[i].name == "Greenville" || towns[i].name == "Springfield") { 
+
+
+            var myArticle = document.createElement('article');
+            var myH2 = document.createElement('h2');
+            var myPara1 = document.createElement('p');
+            var myPara2 = document.createElement('p');
+            var myPara3 = document.createElement('p');
+            var myPara4 = document.createElement('p');
+            var myImages = document.createElement('img');
+
+            myH2.textContent = towns[i].name;
+            myPara1.textContent = towns[i].motto;
+            myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            myPara3.textContent = 'Population: ' + towns[i].currentPopulation;
+            myPara4.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall + '"'; 
+
+            if (towns[i].name == 'Franklin') {
+                myImages.setAttribute('src', 'https://erico1.github.io/assignment/lesson-9/image/townone.jpg');
+                myImages.setAttribute('alt', 'An image of a beatiful city');
+            }
+            else if (towns[i].name == 'Greenville') {
+                myImages.setAttribute('src', 'https://erico1.github.io/assignment/lesson-9/image/towntwo.jpg');
+                myImages.setAttribute('alt', 'An image of a beatiful city');
+            }
+            else if (towns[i].name == 'Springfield') {
+                myImages.setAttribute('src', 'https://erico1.github.io/assignment/lesson-9/image/townthree.jpg');
+                myImages.setAttribute('alt', 'Beautiful city');
+            }
+
+         
+      
+            myArticle.appendChild(myH2);
+
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);
+            myArticle.appendChild(myImages);
+            section.appendChild(myArticle);
+           
+
+
+         } 
+    }
+}
