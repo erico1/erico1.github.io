d05193cac3d77614efe1a80f0bc0262a99df10f7
erico1 <oko15001@byui.edu>
2019-03-21 10:29:54 -0600

    week 10

--- assignment/lesson-10/script/preston.js
+++ assignment/lesson-10/script/preston.js
@@ -38,5 +38,51 @@ ForecastRequest.onload = function () {
     document.getElementById("THUR").innerHTML = Math.round(preston.list[16].main.temp) + " °F";
     document.getElementById("FRI").innerHTML = Math.round(preston.list[32].main.temp) + " °F";
 }
+/*This will be used to display the preston Idaho even*/
+var main = document.querySelector('main');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var towns = request.response;
+    showtownsInfo(towns);
+}
+
+function showtownsInfo(jsonObj) {
+    var towns = jsonObj['towns'];
+    for (var i = 0; i < towns.length; i++) {
+        if (towns[i].name == "Preston" ) { 
+
+
+            var myArticle = document.createElement('article');
+            var myH2 = document.createElement('h2');
+            var myPara1 = document.createElement('p');
+            var myPara2 = document.createElement('p');
+            var myPara3 = document.createElement('p');
+            var myPara4 = document.createElement('p');
+            
+
+            myH2.textContent = towns[i].name + '  Town Event';           
+            myPara2.textContent = towns[i].events[0];
+            myPara3.textContent = towns[i].events[1];
+            myPara4.textContent = towns[i].events[2];
+        
+      
+            myArticle.appendChild(myH2);
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);
+           
+            main.appendChild(myArticle);
+           
+
+
+         } 
+    }
+}
+
 
 
